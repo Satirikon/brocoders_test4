f0e2155acdf308a1b9530b8631134ce77caea9bd
Satirikon <satirikon5216@gmail.com>
2019-04-23 17:13:10 +0300

    Updates

---
+++ precache-manifest.ee229ed88ff066ccecb4dbdc0dfd4bb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e99ea586942d4c2aa90b",
+    "url": "''/static/js/runtime~main.f7063a80.js"
+  },
+  {
+    "revision": "25df070601af57b131ec",
+    "url": "''/static/js/main.e48fa0b5.chunk.js"
+  },
+  {
+    "revision": "c0d35fd0927610d7cb77",
+    "url": "''/static/js/2.eee022a3.chunk.js"
+  },
+  {
+    "revision": "25df070601af57b131ec",
+    "url": "''/static/css/main.73cbe276.chunk.css"
+  },
+  {
+    "revision": "9575b3c28b5c2b47c2748327e5175544",
+    "url": "''/index.html"
+  }
+];
