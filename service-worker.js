bed5a46580df9d0167b98d72cc481a22726ad879
Satirikon <satirikon5216@gmail.com>
2019-04-23 16:16:23 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/brocoders_test4/precache-manifest.f8e6807c051b9008d0cbeeb922379368.js"
+  "/brocoders_test4/precache-manifest.77dbdb10d0f88cc18f397ba943da0deb.js"
 );
 
 workbox.clientsClaim();
